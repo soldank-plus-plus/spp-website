2a4fb060c6292efc64beaffd9216a82a7a46c350
Kuba Zalewski <jakub.zalewski@billo.life>
2024-11-04 23:45:02 +0100

    downgrade nextjs to v14 and setup basic layout with placeholders

---
+++ frontend/src/components/layout/TopNavigation/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TopNavigation = () => {
+  return <p>top navigation</p>;
+};
+
+export default TopNavigation;
--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import { AppCacheProvider } from "@mui/material-nextjs/v15-pagesRouter";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
@@ -5,12 +6,16 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
 
-export default function App({ Component, pageProps }: AppProps) {
+function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <QueryClientProvider client={queryClient}>
-        <Component {...pageProps} />
-      </QueryClientProvider>
+      <AppCacheProvider {...pageProps}>
+        <QueryClientProvider client={queryClient}>
+          <Component {...pageProps} />
+        </QueryClientProvider>
+      </AppCacheProvider>
     </>
   );
 }
+
+export default App;
