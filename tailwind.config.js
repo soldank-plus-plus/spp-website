85d172b915c4047ad15a4a59f05503ca65e1073c
max-ghz <153613487+max-ghz@users.noreply.github.com>
2025-05-30 21:27:07 +0200

    Fix config files due to Prettier config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,30 +3,52 @@ module.exports = {
     darkMode: ["class"],
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
-        extend: {
-            borderRadius: {
-                lg: "var(--radius)",
-                md: "calc(var(--radius) - 2px)",
-                sm: "calc(var(--radius) - 4px)",
-            },
-            fontFamily: {
-                tomorrow: ['"Tomorrow"', "Roboto", "sans-serif"],
-            },
-            fontWeight: {
-                450: "450",
-            },
-            colors: {
-                background: "#000000",
-                foreground: "#ffffff",
-                primary: "#000000",
-                secondary: "#999999",
-                border: "#A6A8B4",
-                heading: "#ffffff",
-                dark: "#9CA3AF",
-                light: "#FFFFFFB3",
-                accent: "#2e4183",
-            },
-        },
+    	extend: {
+    		borderRadius: {
+    			lg: 'var(--radius)',
+    			md: 'calc(var(--radius) - 2px)',
+    			sm: 'calc(var(--radius) - 4px)'
+    		},
+			fontFamily: {
+				tomorrow: ['"Tomorrow"', 'Roboto', 'sans-serif'],
+    		},
+    		fontWeight: {
+    			'450': '450'
+    		},
+    		colors: {
+    			background: '#000000',
+    			foreground: '#ffffff',
+    			primary: '#000000',
+    			secondary: '#999999',
+    			border: '#A6A8B4',
+    			heading: '#ffffff',
+    			dark: '#9CA3AF',
+    			light: '#FFFFFFB3',
+    			accent: '#2e4183'
+    		},
+    		keyframes: {
+    			'accordion-down': {
+    				from: {
+    					height: '0'
+    				},
+    				to: {
+    					height: 'var(--radix-accordion-content-height)'
+    				}
+    			},
+    			'accordion-up': {
+    				from: {
+    					height: 'var(--radix-accordion-content-height)'
+    				},
+    				to: {
+    					height: '0'
+    				}
+    			}
+    		},
+    		animation: {
+    			'accordion-down': 'accordion-down 0.2s ease-out',
+    			'accordion-up': 'accordion-up 0.2s ease-out'
+    		}
+    	}
     },
     plugins: [require("tailwindcss-animate")],
 };
