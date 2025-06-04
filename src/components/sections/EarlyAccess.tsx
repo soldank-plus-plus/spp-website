aaf6752298f6ce8929a676a5978807e2fa874e82
max-ghz <153613487+max-ghz@users.noreply.github.com>
2025-06-04 20:47:00 +0200

    Add temporary layout for sub-pages

---
+++ src/components/sections/EarlyAccess.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Header } from "@/components/sections/Header";
+import { Footer } from "@/components/sections/Footer";
+import underconstr from "@/assets/icons/underconstr.png";
+
+const EarlyAccess = () => {
+    return (
+        <>
+            <Header />
+            <main className="flex justify-center items-center min-h-screen px-4">
+                <img src={underconstr} alt="Under construction" />
+            </main>
+            <Footer />
+        </>
+    );
+};
+
+export default EarlyAccess;
--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,16 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Landing from "@/components/pages/Landing";
 import Gameplay from "@/components/pages/Gameplay";
 import Ranking from "@/components/pages/Ranking";
-import Gamemodes from "@/components/pages/Gamemodes";
 import Maps from "@/components/pages/Maps";
 import Learn from "@/components/pages/Learn";
 import Servers from "@/components/pages/Servers";
 import Faq from "@/components/pages/Faq";
-import Login from "@/components/sections/Login";
-import Signup from "@/components/sections/Signup";
+import Login from "@/components/sections/Authentication/Login";
+import Signup from "@/components/sections/Authentication/Signup";
+import EarlyAccess from "@/components/sections/EarlyAccess";
 import Contributing from "@/components/sections/Contributing";
+import Commits from "@/components/sections/Commits";
+import Roadmap from "@/components/sections/Roadmap";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
@@ -21,14 +23,16 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
                 <Route path="/" element={<Landing />} />
                 <Route path="/gameplay" element={<Gameplay />} />
                 <Route path="/ranking" element={<Ranking />} />
-                <Route path="/gamemodes" element={<Gamemodes />} />
                 <Route path="/maps" element={<Maps />} />
                 <Route path="/learn" element={<Learn />} />
                 <Route path="/servers" element={<Servers />} />
                 <Route path="/faq" element={<Faq />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/signup" element={<Signup />} />
+                <Route path="/earlyaccess" element={<EarlyAccess />} />
                 <Route path="/contributing" element={<Contributing />} />
+                <Route path="/commits" element={<Commits />} />
+                <Route path="/roadmap" element={<Roadmap />} />
             </Routes>
         </BrowserRouter>
     </React.StrictMode>
