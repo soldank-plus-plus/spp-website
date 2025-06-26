2de965f85467b4dd56755c15d51b2b7b5a350490
max-ghz <153613487+max-ghz@users.noreply.github.com>
2025-06-26 17:23:38 +0200

    Add proxy for lobby backend

--- vite.config.ts
+++ vite.config.ts
@@ -9,4 +9,13 @@ export default defineConfig({
             "@": fileURLToPath(new URL("./src", import.meta.url)),
         },
     },
+    server: {
+        proxy: {
+            '/api': {
+                target: 'https://api.soldankpp.app',
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/api/, ''),
+            },
+        },
+    },
 });
