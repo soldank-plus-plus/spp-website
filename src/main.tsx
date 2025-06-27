012b2e81db2ec6cdfe69bbdf4e285bfc7b243708
max-ghz <153613487+max-ghz@users.noreply.github.com>
2025-06-27 12:49:47 +0200

    Update importing

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./index.css";
+
 import Landing from "@/components/pages/Landing";
 import Gameplay from "@/components/pages/Gameplay";
 import Ranking from "@/components/pages/Ranking";
@@ -8,8 +10,10 @@ import Maps from "@/components/pages/Maps";
 import Gamemodes from "@/components/pages/Gamemodes";
 import Servers from "@/components/pages/Servers";
 import Faq from "@/components/pages/Faq";
+
 import Login from "@/components/sections/Authentication/Login";
 import Signup from "@/components/sections/Authentication/Signup";
+
 import EarlyAccess from "@/components/sections/EarlyAccess";
 import Contributing from "@/components/sections/Contributing";
 import Commits from "@/components/sections/Commits";
