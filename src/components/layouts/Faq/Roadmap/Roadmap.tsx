2946637472f56a0d8ca9dd603a0089ef393a2ec8
max-ghz <userbarabiasz95@gmail.com>
2025-12-13 17:00:59 +0100

    Move roadmap components between header and footer

--- src/components/layouts/Faq/Roadmap/Roadmap.tsx
+++ src/components/layouts/Faq/Roadmap/Roadmap.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { Header } from "@/components/sections/Header";
-import { Footer } from "@/components/sections/Footer";
 import RoadmapFeat from "./RoadmapFeat";
 import RoadmapChart from "./RoadmapChart";
+import { Footer } from "@/components/sections/Footer";
 
 export default function Roadmap() {
     return (
