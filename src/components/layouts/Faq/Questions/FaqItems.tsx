c21d4195384f1f6e53c12afb94d322f069cf4c33
max-ghz <userbarabiasz95@gmail.com>
2026-03-31 14:58:54 +0200

    Update questions in FAQ

--- src/components/layouts/Faq/Questions/FaqItems.tsx
+++ src/components/layouts/Faq/Questions/FaqItems.tsx
@@ -6,13 +6,13 @@ export const faqItems = [
         question: "What exactly is Soldank++ about",
         answer: (
             <>
-                Soldank++ is a unique 2D (side-view) arcade game based on the
-                legendary 2D Soldat engine made by Nedi. The main part of the
-                game is based on passing various maps of difficulty with the
-                possibility of achieving faster time than other players. Side
-                modes offer a variety of multiplayer options including shooting
-                competition, hide and seek, dodgeball, zombies and much more.
-                List of full gameplay features can be found{" "}
+                Soldank++ is a unique 2D (side-view) arcade game made by Nedi.
+                The main part of the game is based on passing various maps of
+                difficulty with the possibility of achieving faster time than
+                other players. Side modes offer a variety of multiplayer options
+                including shooting competition, hide and seek, dodgeball,
+                zombies and much more. List of full gameplay features can be
+                found{" "}
                 <a href="/gamemodes" className="underline hover:underline">
                     here
                 </a>
@@ -22,25 +22,38 @@ export const faqItems = [
     },
     {
         id: "item-2",
-        question: "Will gameplay be the same as the original",
+        question: "Is this game a remake of another game",
         answer: (
             <>
-                Yes! That’s the main goal of the project — The primary objective
-                is to replicate all existing core mechanics precisely while
-                expanding upon them. Furthermore, the project is committed to
-                fully replacing certain legacy features, such as netcode, with a
-                server-authoritative system, preventing some existing
-                vulnerabilities.
+                It is an upcoming remake based on the legendary 2D Soldat game,
+                built on a newly rewritten C++ engine. It is not a direct remake
+                of Soldat, but a separate community project with different game
+                purposes that reimplements the original game mechanics and
+                expands them using the new engine.
             </>
         ),
     },
     {
         id: "item-3",
+        question: "Will gameplay be the same as the original game",
+        answer: (
+            <>
+                The core game mechanics, including shooting and movement, will
+                be replicated 1:1. While the mechanics are preserved, the game
+                itself will have a different focus, emphasizing movement over
+                shooting. However, all original game modes will be fully
+                re-implemented and playable in the same way as in the original
+                game.
+            </>
+        ),
+    },
+    {
+        id: "item-4",
         question:
             "Are there any new features or improvements compared to Soldat",
         answer: (
             <>
-                As of now, several new features and improvements have been
+                In early access, several new features and improvements have been
                 introduced compared to the original game. Most of the core
                 mechanics have been rewritten in C++, including a completely
                 overhauled movement system based on a finite state machine —
@@ -48,41 +61,37 @@ export const faqItems = [
                 changes. A new in-game map editor has also been added, allowing
                 you to test maps instantly as you build. On top of that, we now
                 have a fully functional lobby server and an official website.
-                Follow us on social media to stay updated on the latest
-                developments.
-            </>
-        ),
-    },
-    {
-        id: "item-4",
-        question: "What platforms are supported (Windows/Linux/macOS/Web)",
-        answer: (
-            <>
-                The game will be playable on all major platforms — Windows,
-                Linux, macOS, and the Web. The technologies used in the project
-                are cross-platform. The latest binaries are available for
-                download (macOS should work too but it was not tested).
             </>
         ),
     },
     {
         id: "item-5",
-        question: "How do I install the game",
+        question: "What platforms are supported (Windows/Linux/macOS/Web)",
         answer: (
             <>
-                Latest build of the game is available at:{" "}
-                <a
-                    href="https://github.com/soldank-plus-plus/soldank-plus-plus/releases/tag/release-latest"
-                    className="underline hover:underline"
-                >
-                    https://github.com/soldank-plus-plus/soldank-plus-plus/releases/tag/release-latest
-                </a>
-                .
+                The game will be playable on all major platforms — Windows,
+                Linux, macOS, and the Web. The technologies used in the project
+                are cross-platform (macOS should work too but it was not
+                tested).
             </>
         ),
     },
     {
         id: "item-6",
+        question: "How do I install the game",
+        answer: (
+            <>
+                Latest builds of the game are available at{" "}
+                <a href="/earlyaccess" className="underline hover:underline">
+                    Early Access
+                </a>{" "}
+                page. You can download it for Windows and Linux. macOS is not
+                available as it has not been fully tested.
+            </>
+        ),
+    },
+    {
+        id: "item-7",
         question: "How can I contribute",
         answer: (
             <>
@@ -101,6 +110,17 @@ export const faqItems = [
                     here
                 </a>
                 !
+            </>
+        ),
+    },
+    {
+        id: "item-8",
+        question: "When is stable version coming out",
+        answer: (
+            <>
+                There is no specific release date, as this project is developed
+                in free time. Even if progress isn’t always visible, the project
+                is being worked on and is not abandoned.
             </>
         ),
     },
