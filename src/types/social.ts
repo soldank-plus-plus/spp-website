c6a0c7cbfdaaaece157e8877974b75dd9254d437
max-ghz <userbarabiasz95@gmail.com>
2026-08-25 16:09:53 +0200

    Add UserSocials type

---
+++ src/types/social.ts
@@ -0,0 +1,8 @@
+export type UserSocials = {
+    id: number;
+    user_id: number;
+    twitch: string | null;
+    youtube: string | null;
+    github: string | null;
+    discord: string | null;
+};
