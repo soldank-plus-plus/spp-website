128adf0b7d2ae35964013baabb0496ab74b42416
max-ghz <userbarabiasz95@gmail.com>
2026-09-09 23:28:20 +0200

    Serve the Tomorrow font from the app

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import ScrollToTop from "@/hooks/core/ScrollToTop";
+import "@fontsource/tomorrow/400.css";
+import "@fontsource/tomorrow/500.css";
+import "@fontsource/tomorrow/700.css";
 import "./index.css";
 
 import { appRoutes, AppRoute } from "@/config/Routes";
